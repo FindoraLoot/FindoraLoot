c113dc7b10ecad3368ae2488bb6093c417034498
cryptoKitty888 <109696581+cryptoKitty888@users.noreply.github.com>
2022-07-20 16:30:39 -0700

    Update tokens.ts

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -3,6 +3,6 @@ export default {
     lootAddress: '0xDB0C633d92e9a2A81ab8B58087dAf2FE796f6fA5',
   },
   dev: {
-    lootAddress: '0x1f4bEF3A39Cb9fD35cb221500b383E2d6b56C9D7',
+    lootAddress: '0x6c310e01980E51789F9d31f4d4b7912AAF93c4dc',
   },
 };
